fba1a55e72d01f0be8db3fd974d97feb4fa48e43
Armin Kuburas <akuburas@c1r5p9.hive.fi>
2024-06-25 14:01:18 +0300

    start of ray tracing test

---
+++ testing_zone/ray_tracing_test_01.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_tracing_test_01.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 13:39:06 by akuburas          #+#    #+#             */
+/*   Updated: 2024/06/25 14:00:00 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include "../../MLX42/include/MLX42/MLX42.h"
+
+int	main(void)
+{
+	
+}
