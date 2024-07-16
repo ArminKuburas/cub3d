1549233f2b9b63003c09247f1b555d3cf9e22a93
ArminKuburas <akuburass@gmail.com>
2024-07-17 02:07:41 +0300

    feat: Adding the initial main file as well as some others

---
+++ first_attempt/source_files/main.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 00:52:48 by akuburas          #+#    #+#             */
+/*   Updated: 2024/07/17 02:04:08 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "../other_libraries/libft/libft.h"
+
+#define FAILURE 1
+#define SUCCESS 0
+
+int	ft_err(char *str)
+{
+	write(2, "Error\n", 6);
+	write(2, str, ft_strlen(str));
+	return (FAILURE);
+}
+
+int	check_map_name(char *str)
+{
+	char	*find_last_slash;
+	char	*find_dot;
+
+	find_last_slash = ft_strchr(str, '/');
+	if (!find_last_slash)
+		find_dot = ft_strchr(str, '.');
+	else
+	{
+		find_last_slash = ft_strrchr(str, '/');
+		find_dot = ft_strrchr(find_last_slash, '.');
+	}
+	if (!find_dot)
+		return (FAILURE);
+	if (ft_strcmp(find_dot, ".cub") != 0)
+		return (FAILURE);
+	return (SUCCESS);
+}
+
+int	initial_checks(int argc, char **argv)
+{
+	if (argc != 2)
+		return (ft_err("Wrong number of arguments\nUsage: ./cub3D <map>.cub\n"));
+	if (check_map_name(argv[1]) == FAILURE)
+		return (ft_err("Given incorrect file format\n"));
+}
+
+/**
+ * This message is for my teammate. I know you did a lot of map handling already
+ * But I decided I wanted to do this small bit as a starting point for this file
+ * We can modify/remove if needed.
+ */
+int	main(int argc, char **argv)
+{
+	if (initial_checks(argc, argv) == FAILURE)
+		return (FAILURE);
+	(void)argc;
+	(void)argv;
+	return (0);
+}
