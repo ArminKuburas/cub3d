ea21ac7ea12e51650d2f6217b1210519a61351fd
ArminKuburas <akuburass@gmail.com>
2024-07-17 02:49:49 +0300

    feat: began work on the data struct

---
+++ first_attempt/headers/cub3d.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 02:30:52 by akuburas          #+#    #+#             */
+/*   Updated: 2024/07/17 02:41:43 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+#include "../../../MLX42/include/MLX42/MLX42.h"
+
+typedef struct s_data
+{
+	mlx_t			*mlx;
+	mlx_image_t		*image;
+	mlx_texture_t	*north_texture;
+	mlx_texture_t	*south_texture;
+	mlx_texture_t	*west_texture;
+	mlx_texture_t	*east_texture;
+}	t_data;
+
+#endif
