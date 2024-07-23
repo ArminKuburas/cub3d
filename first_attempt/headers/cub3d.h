b44aa1f2cd0d6b17c1100630abb89ae359daf652
Andrey Kovalevskiy <akovalev@student.hive.fi>
2024-07-23 17:39:10 +0300

    contunuing parsing restructuring and norm cleanup

--- first_attempt/headers/cub3d.h
+++ first_attempt/headers/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: akovalev <akovalev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 02:30:52 by akuburas          #+#    #+#             */
-/*   Updated: 2024/07/22 18:12:28 by akovalev         ###   ########.fr       */
+/*   Updated: 2024/07/23 17:23:41 by akovalev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,8 @@ typedef struct s_map
 	char		**ceiling;
 	char		**floor;
 	int			start_pos;
+	char		*pr_str;
+	char		*nxt_str;
 }	t_map;
 
 typedef struct s_data
@@ -87,6 +89,9 @@ typedef struct s_parsing_data
 	char	*ceiling_colour;
 }	t_parsing_data;
 
-int	ft_err(char *str);
+int		ft_err(char *str);
+int		check_arguments(int argc, char **argv, t_map *map);
+void	free_map_info(t_map *map);
+int		validate_map(t_map *map);
 
 #endif
